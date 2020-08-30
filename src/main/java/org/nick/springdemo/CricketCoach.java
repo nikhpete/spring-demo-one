848302326fa65dbdf84db207166354cb8cc01743
Nikhil Joseph Peter <josef.nikhil18@gmail.com>
2020-08-30 13:39:56 +0530

    Inject string literals

--- src/main/java/org/nick/springdemo/CricketCoach.java
+++ src/main/java/org/nick/springdemo/CricketCoach.java
@@ -5,11 +5,23 @@ import org.nick.springdemo.service.FortuneService;
 public class CricketCoach implements Coach {
 
 	private FortuneService fs; 
-
+	private String emailAddress;
+	private String team;
 
 	public void setFs(FortuneService fs) {
-		System.out.println("Inside setter method");
+		System.out.println("Inside fs setter method");
 		this.fs = fs;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("Inside emailAddress setter method");
+		this.emailAddress = emailAddress;
+	}
+
+
+	public void setTeam(String team) {
+		System.out.println("Inside team setter method");
+		this.team = team;
 	}
 
 	public CricketCoach() {
@@ -18,6 +30,8 @@ public class CricketCoach implements Coach {
 
 	public String getDailyWorkout() {
 		System.out.println(fs.getFortune());
+		System.out.println(emailAddress);
+		System.out.println(team);
 		return "Practise fast bowling for 15 minutes";
 	}
 
