02547675b9893200684a06620699d328c715b842
Nikhil Joseph Peter <josef.nikhil18@gmail.com>
2020-08-30 16:16:52 +0530

    inject value from file and random fortune

--- src/main/java/org/nick/springdemo/service/HappyFortuneService.java
+++ src/main/java/org/nick/springdemo/service/HappyFortuneService.java
@@ -1,9 +1,17 @@
 package org.nick.springdemo.service;
 
+import java.util.Random;
+
 public class HappyFortuneService implements FortuneService {
+	
+	private String[] data = { 
+			"Beware of the wolf in sheep's clothing",
+			"Diligence is the mother of good luck",
+			"The journey is the reward"
+	};
 
 	public String getFortune() {
-		return "Today is ur lucky day";
+		return data[new Random().nextInt(data.length)];
 	}
 
 }
